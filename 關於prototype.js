45caf2b7c890dcb0cec40b1ddc9ec5693955ac2e
Tingan <syeak123@gmail.com>
2025-03-21 20:03:47 +0800

    update:add class training

--- 關於prototype.js
+++ 關於prototype.js
@@ -26,3 +26,26 @@ const p4 =new Person1("Bob");
 
 console.log(p3.sayHello===p4.sayHello);
 //true
+console.log(Object.getPrototypeOf(p3.sayHello)); 
+
+
+//語法糖
+class xyz {
+    constructor(x,y,z){
+    this.x=x;
+    this.y=y;
+    this.z=z;
+
+}}
+const a=new xyz();
+a.x=1;
+a.y=2;
+a.z=3;
+console.log(a);
+
+const b=new xyz(1,2,3);
+console.log(b.x+a.x);
+console.log(a===b);
+
+
+
