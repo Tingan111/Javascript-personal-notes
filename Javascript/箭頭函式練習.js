4c816b8b36bc4ac875377decf9049fdb1b238638
Tingan <syeak123@gmail.com>
2025-04-01 15:34:33 +0800

    update:箭頭函數練習.js

--- Javascript/箭頭函式練習.js
+++ Javascript/箭頭函式練習.js
@@ -1,4 +1,9 @@
-const materials = ["Html", "CSS", "Javascript", "React"];
-console.log(materials.map((materials)=>materials.length));
-// [4,3,10,5]
+// const materials = ["Html", "CSS", "Javascript", "React"];
+// console.log(materials.map((materials)=>materials.length));
+// // [4,3,10,5]
+//用map抓陣列每個字串轉換成字串長度
+
+const numbers = [1, 2, 3, 4, 5];
+const doubled=numbers.map(x=>x*2)
+console.log(doubled); // 預期輸出：[2, 4, 6, 8, 10]
 
