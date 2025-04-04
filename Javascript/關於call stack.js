7572ed04e3bf8ff24534de5c443a3673060635fb
Tingan <syeak123@gmail.com>
2025-04-04 21:51:52 +0800

    fix:Change file name

--- Javascript/關於call stack.js
+++
@@ -1,7 +0,0 @@
-const a=()=>{console.log("我是A"),b()}
-const b=()=>{console.log('我是B'),c()}
-const c=()=>{console.log('我是C');
-}
-a()
-
-//call stack
---
+++ Javascript/關於callstack.js
@@ -0,0 +1,8 @@
+const b=()=>{console.log('我是B'),c()}
+const c=()=>{console.log('我是C');}
+const a=()=>{console.log("我是A"),b()}
+
+a()
+
+//call stack
+
