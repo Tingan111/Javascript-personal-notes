6588a90c7b45589b5df28baf19fd66bea02e3a7c
Tingan <syeak123@gmail.com>
2025-06-29 21:39:56 +0800

    docs:補充隨機選取中獎者的註解

--- Javascript/隨機抽取.js
+++ Javascript/隨機抽取.js
@@ -1,3 +1,6 @@
 const users=["小明","小花","小草","小小","小華","小黑","小白"]
 const winner=users[Math.floor(Math.random()*users.length)]
 console.log("中獎者",winner);
+// Math.random() 會產生 0～1 之間的小數，乘上陣列長度後
+// 仍是小數，需用 Math.floor() 無條件捨去成整數索引
+// 才能正確取出陣列內的元素
