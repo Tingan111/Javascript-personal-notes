c952608c99aa805de2794cbfbf71be34c098d421
Tingan <syeak123@gmail.com>
2025-07-06 20:50:48 +0800

    feat:feat: 練習 map 基礎語法

--- Javascript/r.js
+++ Javascript/r.js
@@ -1,10 +1,7 @@
-const numbers=[1,2,3,4,5]
-for(let number of numbers){
-  console.log(number);
-  
-}
+const arr=[1,2,3,4,5]
 
-for(let number in numbers){
-  console.log(number);
-  
-}
+const result=(arr)=>arr.map(arr=>arr*2)
+
+console.log(result(arr));//[ 2, 4, 6, 8, 10 ]
+
+
