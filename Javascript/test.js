deab4450780ee141517d2b9e4cc5a5dcc15f8761
Tingan <syeak123@gmail.com>
2025-07-08 17:41:35 +0800

    feat:用map+控制流程判斷式改變陣列物件的布林值

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,10 +1,15 @@
 const todos = [
-    { id: 1, text: '買牛奶', done: false },
-    { id: 2, text: '寫作業', done: true },
-    { id: 3, text: '整理房間', done: false },
-  ];
+  { id: 1, text: "買牛奶", done: false },
+  { id: 2, text: "寫作業", done: true },
+  { id: 3, text: "整理房間", done: false },
+];
 
-const deleteTodo=(text)=>todos.filter(todo=>todo.text!==text)
-
-console.log(deleteTodo('買牛奶'));
-console.log(todos[2].text);
+const test = (id) =>
+  todos.map((todo) => {
+    if (todo.id === id) {
+      return { ...todo, done: true };
+    } else {
+      return todo;
+    }
+  });
+console.log(test(1));
