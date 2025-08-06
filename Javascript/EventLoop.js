e39c66bc6bbb99ab966d7d98a49a45bb4e7ae7a2
Tingan <syeak123@gmail.com>
2025-08-06 09:55:49 +0800

    docs: update event loop content

--- Javascript/EventLoop.js
+++ Javascript/EventLoop.js
@@ -7,11 +7,11 @@ Promise.resolve().then(()=>{
 });
 console.log("4.結束");
 
-//觀察顯示順序 會出現 1 4 3 2 為什麼？
-//同步 > 微任務 > 宏任務
-//同步任務：會直接執行，排在最前面。console.log() fuction啟動
+// 同步任務：會直接執行，排在最前面。
 
-//微任務（Microtask）：像是 Promise.then()、MutationObserver，會在當前執行堆疊結束後立刻執行，優先於宏任務。
+// 微任務（Microtask）：像是 Promise.then()、MutationObserver，
+// - 會在目前所有同步任務結束後執行，
+// - 而且會「一次清空所有微任務」後，才輪到宏任務。
 
-//宏任務（Macrotask）：像是 setTimeout、setInterval、DOM event 等。
-
+// 宏任務（Macrotask）：像是 setTimeout、setInterval、DOM 事件的回調函數等，
+// - 每次事件循環會處理一個宏任務，然後再清光微任務。
