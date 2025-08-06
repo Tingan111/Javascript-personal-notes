71adc1df0faffd4f1af9444c81e736fb54c79221
Tingan <syeak123@gmail.com>
2025-08-06 13:58:54 +0800

    feat: update EventLoop content

--- Javascript/EventLoop.js
+++ Javascript/EventLoop.js
@@ -1,17 +1,23 @@
-(()=> console.log("標題"))()
-console.log("1.開始");
-setTimeout(()=>{
-    console.log("2.執行setTimeout")},0);
-Promise.resolve().then(()=>{
-    console.log("3.Promise");
-});
-console.log("4.結束");
+(() => {
+    console.log("同步1");
+  
+    setTimeout(() => {
+      console.log("宏任務1");
+    }, 0);
+  
+    Promise.resolve().then(() => {
+      console.log("微任務1");
+    });
+  
+    Promise.resolve().then(() => {
+      console.log("微任務2");
+    });
+  
+    setTimeout(() => {
+      console.log("宏任務2");
+    }, 0);
+  
+    console.log("同步2");
+  })();
 
-// 同步任務：會直接執行，排在最前面。
-
-// 微任務（Microtask）：像是 Promise.then()、MutationObserver，
-// - 會在目前所有同步任務結束後執行，
-// - 而且會「一次清空所有微任務」後，才輪到宏任務。
-
-// 宏任務（Macrotask）：像是 setTimeout、setInterval、DOM 事件的回調函數等，
-// - 每次事件循環會處理一個宏任務，然後再清光微任務。
+//事件循環 = 同步任務 ➜ 清空所有微任務 ➜ 執行 1 個宏任務 ➜ 再次循環
